abeb8d8e08094691ba4cce53f3f6bf832c9e51be
brunner_f <fabian.brunner@brunnerf.com>
2021-11-12 21:19:29 +0100

    implement graphical interface and bugfixes

--- programm/src/businesLogic/MarketService.java
+++ programm/src/businesLogic/MarketService.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import model.CryptoCurrency;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 @Getter
@@ -15,19 +16,31 @@ public class MarketService {
     private List<CryptoCurrency> currencies = StaticLoader.currencies;
     private double cash = 10000;
 
-    public void buy(CryptoCurrency currency ,Double investedMoney) throws Exception {
-        if(investedMoney > cash) {
+    public void buy(CryptoCurrency currency, Double investedMoney) throws Exception {
+        if (roundDouble(investedMoney).equals(roundDouble(cash))) {
+            investedMoney = cash;
+        }
+        if (investedMoney > cash) {
             throw new Exception("You don't have enough money!");
         }
         cash -= investedMoney;
         currency.buy(investedMoney);
     }
 
-    public void sell(CryptoCurrency currency ,Double amount) throws Exception {
-        if(amount > currency.getHolding()) {
-            throw new Exception("You don't have enough money!");
+    public void sell(CryptoCurrency currency, Double amount) throws Exception {
+        if (roundDouble(amount).equals(roundDouble(currency.getHolding()))) {
+            amount = currency.getHolding();
+        }
+        if (amount > currency.getHolding()) {
+            throw new Exception("You don't have enough " + currency.getName() + "!");
         }
         cash += currency.sell(amount);
+    }
+
+    private String roundDouble(Double number) {
+        DecimalFormat df = new DecimalFormat();
+        df.setMaximumFractionDigits(2);
+        return df.format(number);
     }
 
 }
--- programm/src/coinInterface/buy.java
+++
@@ -1,9 +0,0 @@
-package coinInterface;
-
-import javax.swing.*;
-
-public class buy {
-    private JButton buyButton;
-    private JButton cancelButton;
-    private JTextField textField1;
-}
